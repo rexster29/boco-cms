7c899d92d94d2d84dbea4c138f8008dd7844dfae
absol <72121483+Abstract-ak@users.noreply.github.com>
2025-02-24 09:48:38 +0530

    Update server.ts

--- boco-backend/config/server.ts
+++ boco-backend/config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
-  host: env('0.0.0.0'),
-  port: env.int(1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
